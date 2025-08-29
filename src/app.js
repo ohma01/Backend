c2824e3d6b080bb8c7d6e656b10ba9bb62ace518
kumarAmit0109 <pandeyamit01092002@gmail.com>
2025-08-29 06:19:38 +0530

    configuration added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,31 @@
-import express from "express";
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+// configuring cors 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+// configuration for receiving the json data and setting limit on it
+app.use(express.json({
+    limit:"20kb"
+}))
+
+// configuration for receiving the data from the url and for receivinng 
+// the nested Object we keep extended equals to true
+app.use(urlencoded({
+    extended : true,
+    limit : "20kb"
+}))
+
+// configuration for storing files, img,  favicon, etc on the server in public folder
+app.use(express.static("public"))
+
+// configuration for performing crud operation on the cookie stored on client side
+app.use(cookieParser());
 
 export default app;
